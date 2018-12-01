af057562beaac09b5ea3613dc18036c91cfa0619
Dmitriy Davydov <dvdvdmt@gmail.com>
2018-12-01 19:33:51 +0500

    Set up proper extension building

--- src/background.js
+++ src/background.js
@@ -3,3 +3,5 @@ import browser from 'webextension-polyfill';
 browser.browserAction.onClicked.addListener(() => {
   console.log('It works!');
 });
+
+console.log('It loads!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,37 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+const conf = {
+  mode: 'development',
 
   entry: {
     background: './src/background.js',
   },
 
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
 
-  mode: 'development',
+  devtool: 'eval-source-map',
+
+  plugins: [
+    new CopyWebpackPlugin([
+      'src/manifest.json',
+      {
+        from: 'src/images/',
+        to: 'images',
+      },
+      'src/popup.html',
+    ]),
+  ],
+
+};
+
+module.exports = (env, argv) => {
+  if (env.production) {
+    conf.mode = argv.mode;
+    conf.devtool = 'source-map';
+  }
+  return conf;
 };
