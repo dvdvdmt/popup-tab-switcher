f4df1097312399f779dfece6cc1809c68bdc6d2a
Dmitriy Davydov <dvdvdmt@gmail.com>
2018-12-05 09:45:56 +0500

    Execute content script just once

--- src/background.js
+++ src/background.js
@@ -1,12 +1,19 @@
 import browser from 'webextension-polyfill';
 
+const initializedTabs = {};
+
 browser.commands.onCommand.addListener(async (command) => {
-  await browser.tabs.insertCSS({ file: 'content.css' });
-  await browser.tabs.executeScript({ file: 'content.js' });
   const [currentTab] = await browser.tabs.query({
     active: true,
     currentWindow: true,
   });
+
+  if (!initializedTabs[currentTab.id]) {
+    await browser.tabs.insertCSS({ file: 'content.css' });
+    await browser.tabs.executeScript({ file: 'content.js' });
+    initializedTabs[currentTab.id] = currentTab;
+  }
+
   const allTabs = await browser.tabs.query({});
   const message = {
     tabs: allTabs.map(({ url, title, favIconUrl }) => ({
