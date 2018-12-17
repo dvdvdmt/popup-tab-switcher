cbd8a12904bae69867182e2800b6fe9a932b434f
Dmitriy Davydov <dvdvdmt@gmail.com>
2018-12-17 12:04:54 +0500

    Add Puppeteer for e2e testing

---
+++ src/e2e-tests/e2e.test.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const pathToExtension = path.join(__dirname, '../../dist');
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 500,
+    args: [
+      `--disable-extensions-except=${pathToExtension}`,
+      `--load-extension=${pathToExtension}`,
+    ],
+  });
+  const [page] = await browser.pages();
+  await page.goto('https://www.wikipedia.org');
+  await page.keyboard.down('Alt');
+  await page.keyboard.press('KeyY');
+})();
