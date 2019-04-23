82ed9e0da60809cee96366959063faede3a32348
Dmitriy Davydov <dvdvdmt@gmail.com>
2019-04-23 16:52:41 +0500

    Fix source maps
    https://www.notion.so/dvdvdmt/Popup-tab-switcher-ecf729f6ab6d4d55bce2b3e3d9c00342?p=c76d9d875a2c4f2aab475c9d11c89cff

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const conf = {
     path: path.resolve(__dirname, 'dist'),
   },
 
-  devtool: 'cheap-module-source-map',
+  devtool: 'eval-source-map',
 
   plugins: [
     new CopyWebpackPlugin([
