b3701469576358caedcab0b70c621e2dc7f11713
Dmitriy Davydov <dvdvdmt@gmail.com>
2019-06-15 17:20:08 +0500

    Apply color theme to the settings popup dynamically
    https://www.notion.so/dvdvdmt/Popup-tab-switcher-ecf729f6ab6d4d55bce2b3e3d9c00342?p=c8686fef9fa3481ea5240f47327588eb

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="settings">
-    <label class="settings-row">
+  <div class="settings mdc-typography" v-bind:class="{settings_dark: settings.isDarkTheme}">
+    <m-top-app-bar>Settings</m-top-app-bar>
+    <label class="settings__row">
       <m-switch v-model="settings.isDarkTheme"></m-switch>
       {{msg}}
     </label>
@@ -11,34 +12,49 @@
 <script>
   import * as settings from '../utils/settings';
   import MSwitch from './components/MSwitch.vue';
+  import MTopAppBar from './components/MTopAppBar.vue';
 
   export default {
     name: 'Popup',
     data() {
       return {
         msg: 'Dark theme',
-        settings: settings.get()
+        settings: settings.get(),
       };
     },
     created() {
       function saveSettings() {
-        settings.update(this.settings)
+        settings.update(this.settings);
       }
 
       this.$watch('settings', saveSettings, { deep: true });
     },
     components: {
-      MSwitch
-    }
+      MSwitch,
+      MTopAppBar,
+    },
   };
 </script>
 
 <style lang="scss">
-  .settings {
-    width: 200px;
+  @import '~@material/typography/mdc-typography';
+
+  body {
+    margin: 0;
   }
 
-  .settings-row {
+  .settings {
+    @include settings-theme-light();
+
+    width: 200px;
+    background-color: var(--settings-background-color);
+
+    &_dark {
+      @include settings-theme-dark();
+    }
+  }
+
+  .settings__row {
     display: flex;
     justify-content: left;
     align-items: center;
---
+++ src/popup/components/MTopAppBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="mdc-top-app-bar mdc-top-app-bar--dense">
+    <div class="mdc-top-app-bar__row">
+      <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+        <span class="mdc-top-app-bar__title"><slot></slot></span>
+      </section>
+    </div>
+  </header>
+</template>
+
+<script>
+  import { MDCTopAppBar } from '@material/top-app-bar/index';
+
+  export default {
+    name: 'MTopAppBar',
+    mounted() {
+      MDCTopAppBar.attachTo(this.$el);
+    }
+  };
+</script>
+
+<style lang="scss">
+  @import "~@material/top-app-bar/mdc-top-app-bar.scss";
+
+  .mdc-top-app-bar {
+    position: initial;
+  }
+</style>
