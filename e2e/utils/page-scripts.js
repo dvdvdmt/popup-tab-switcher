c639c4f25290458dcff0491c1251a239b50a25e2
Dmitriy Davydov <dvdvdmt@gmail.com>
2020-12-11 00:11:04 +0500

    #14 Improve isVisible() utility.
    Handle penetration of shadow roots.
    Replace unnecessary usages of offsetHeight and offsetWidth to more reliable BoundingClinetRect's width and height.
    Remove usages of window's innerHeight and innerWidth in favor of documentElement's clientHeight and clientWidth because their values don't include scrollbar size.

--- e2e/utils/page-scripts.js
+++ e2e/utils/page-scripts.js
@@ -1,10 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function registerScripts() {
-  function isVisible(elem) {
-    if (!(elem instanceof Element)) {
-      throw Error('DomUtil: elem is not an element.');
+  function getElementFromPoint({x, y}) {
+    // Penetrates shadow roots
+    let el = document.elementFromPoint(x, y);
+    while (el.shadowRoot) {
+      el = el.shadowRoot.elementFromPoint(x, y);
     }
-    const style = getComputedStyle(elem);
+    return el;
+  }
+
+  function isVisible(el) {
+    const style = getComputedStyle(el);
     if (style.display === 'none') {
       return false;
     }
@@ -14,38 +20,30 @@ function registerScripts() {
     if (style.opacity < 0.1) {
       return false;
     }
+    const elRect = el.getBoundingClientRect();
+    if (elRect.height === 0 && elRect.width === 0) {
+      return false;
+    }
+    const elCenter = {
+      x: elRect.left + elRect.width / 2,
+      y: elRect.top + elRect.height / 2,
+    };
     if (
-      elem.offsetWidth +
-        elem.offsetHeight +
-        elem.getBoundingClientRect().height +
-        elem.getBoundingClientRect().width ===
-      0
+      elCenter.x < 0 ||
+      elCenter.x > document.documentElement.clientWidth ||
+      elCenter.y < 0 ||
+      elCenter.y > document.documentElement.clientHeight
     ) {
       return false;
     }
-    const elemCenter = {
-      x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
-      y: elem.getBoundingClientRect().top + elem.offsetHeight / 2,
-    };
-    if (elemCenter.x < 0) {
-      return false;
-    }
-    if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) {
-      return false;
-    }
-    if (elemCenter.y < 0) {
-      return false;
-    }
-    if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) {
-      return false;
-    }
-    let pointContainer = document.elementFromPoint(elemCenter.x, elemCenter.y);
-    do {
-      if (pointContainer === elem) {
+    let pointContainer = getElementFromPoint(elCenter);
+    while (pointContainer) {
+      if (pointContainer === el) {
         return true;
       }
-      pointContainer = pointContainer.parentNode;
-    } while (pointContainer);
+      pointContainer =
+        pointContainer instanceof ShadowRoot ? pointContainer.host : pointContainer.parentNode;
+    }
     return false;
   }
 
