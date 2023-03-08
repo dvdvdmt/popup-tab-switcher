54c544163149450b333a98afa0838f9cd2b5ac66
Dmitriy Davydov <dvdvdmt@gmail.com>
2023-03-08 08:19:07 +0400

    Restore selection of the text inside "iframe" element

--- src/content-script/selection-and-focus.ts
+++ src/content-script/selection-and-focus.ts
@@ -1,20 +1,15 @@
-export interface IInputElementSelection {
-  start: number
-  end: number
-  direction: 'forward' | 'backward' | 'none'
-  element: HTMLInputElement | HTMLTextAreaElement
-}
-
 export class SelectionAndFocus {
-  activeElement: Element | null = null
+  private activeWindow: Window | null
 
-  selectionRanges: Range[] = []
+  private activeElement: Element | null = null
 
-  inputElementSelection: IInputElementSelection | null = null
+  private selectionRanges: Range[] = []
+
+  private inputElementSelection: IInputElementSelection | null = null
 
   static getInputElementSelection(element: Element | null): IInputElementSelection | null {
     if (
-      !(element instanceof HTMLInputElement || element instanceof HTMLTextAreaElement) ||
+      !isInputOrTextarea(element) ||
       element.selectionStart == null ||
       element.selectionEnd == null
     ) {
@@ -26,8 +21,8 @@ export class SelectionAndFocus {
     return {start, end, direction, element}
   }
 
-  static getSelectionRanges(): Range[] {
-    const selection = window.getSelection()
+  static getSelectionRanges(window: Window | null): Range[] {
+    const selection = window?.getSelection()
     if (!selection) {
       return []
     }
@@ -38,9 +33,24 @@ export class SelectionAndFocus {
     return ranges
   }
 
+  /**
+   * Gets the window object of the active element.
+   * It pierces through all iframes recursively.
+   */
+  static getActiveWindow(current: Window): Window | null {
+    const activeElement = current.document.activeElement
+    if (!activeElement) {
+      return null
+    }
+    if (activeElement instanceof HTMLIFrameElement && activeElement.contentWindow) {
+      return SelectionAndFocus.getActiveWindow(activeElement.contentWindow)
+    }
+    return current
+  }
+
   /** Applies current selection and focus state to the page. */
   apply() {
-    if (!this.activeElement) {
+    if (!this.activeWindow || !this.activeElement) {
       return
     }
 
@@ -62,7 +72,7 @@ export class SelectionAndFocus {
     }
 
     if (this.selectionRanges.length > 0) {
-      const selection = window.getSelection()
+      const selection = this.activeWindow.getSelection()
       if (selection) {
         selection.removeAllRanges()
         this.selectionRanges.forEach((range) => selection.addRange(range))
@@ -73,18 +83,33 @@ export class SelectionAndFocus {
   }
 
   saveState() {
-    this.activeElement = this.activeElement ?? document.activeElement
+    this.activeWindow = this.activeWindow ?? SelectionAndFocus.getActiveWindow(window)
+    this.activeElement = this.activeElement ?? this.activeWindow?.document.activeElement ?? null
     this.selectionRanges =
       this.selectionRanges.length > 0
         ? this.selectionRanges
-        : SelectionAndFocus.getSelectionRanges()
+        : SelectionAndFocus.getSelectionRanges(this.activeWindow)
     this.inputElementSelection =
       this.inputElementSelection ?? SelectionAndFocus.getInputElementSelection(this.activeElement)
   }
 
   resetState() {
+    this.activeWindow = null
     this.activeElement = null
     this.selectionRanges = []
     this.inputElementSelection = null
   }
+}
+
+export interface IInputElementSelection {
+  start: number
+  end: number
+  direction: 'forward' | 'backward' | 'none'
+  element: HTMLInputElement | HTMLTextAreaElement
+}
+
+function isInputOrTextarea(
+  element: Element | null
+): element is HTMLInputElement | HTMLTextAreaElement {
+  return element !== null && (element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA')
 }
