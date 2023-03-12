893fe248fcd13a45a22a248ba783379e5aec55dd
Dmitriy Davydov <dvdvdmt@gmail.com>
2023-03-12 11:16:26 +0400

    Add another test of the switcher popup on custom settings

---
+++ e2e/utils/page-scripts/set-settings.ts
@@ -0,0 +1,7 @@
+import {e2eSetSettings} from '../../../src/utils/messages'
+import {sendMessage} from './send-message'
+import {ISettings} from '../../../src/utils/settings'
+
+export async function setSettings(settings: Partial<ISettings>): Promise<void> {
+  await sendMessage(e2eSetSettings(settings))
+}
--- src/background.ts
+++ src/background.ts
@@ -75,6 +75,11 @@ async function initForE2ETests(handlers: Partial<IHandlers>) {
     [Message.E2E_SET_ZOOM]: ({zoomFactor}) => {
       browser.tabs.setZoom(zoomFactor)
     },
+    [Message.E2ESetSettings]: async ({settings}) => {
+      log(`[Settings received]`, settings)
+      const currentSettings = await ServiceFactory.getSettings()
+      await currentSettings.update(settings)
+    },
     [Message.E2E_RELOAD_EXTENSION]: async () => {
       await browser.runtime.reload()
     },
