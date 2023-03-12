71c96b2832fc4232292ad7ddbeece3018d647b6b
Dmitriy Davydov <dvdvdmt@gmail.com>
2023-03-12 13:30:20 +0400

    Increase timeout to wait for animation to complete

--- e2e/features/settings-view/settings-view.test.ts
+++ e2e/features/settings-view/settings-view.test.ts
@@ -1,7 +1,13 @@
 import path from 'path'
-import {startPuppeteer, stopPuppeteer, timeoutDurationMS} from '../../utils/puppeteer-utils'
+import {
+  startPuppeteer,
+  stopPuppeteer,
+  timeoutDurationMS,
+  waitFor,
+} from '../../utils/puppeteer-utils'
 import {PuppeteerPopupHelper} from '../../utils/puppeteer-popup-helper'
 import {settingsPage} from '../../selectors/settings-page'
+import {defaultSettings} from '../../../src/utils/settings'
 
 /**
  * Contains visual tests of the settings component.
@@ -17,6 +23,14 @@ describe('Settings view', function () {
   )
 
   after(stopPuppeteer)
+
+  afterEach(async () => {
+    // reset settings to default
+    const page = await helper.getActivePage()
+    await page.evaluate((settings) => {
+      window.e2e.setSettings(settings)
+    }, defaultSettings)
+  })
 
   it(`settings page on defaults looks as expected`, async () => {
     // Given the settings page is open.
@@ -45,6 +59,10 @@ describe('Settings view', function () {
     // Given the contribute page is open with default settings.
     const page = await helper.openPage('settings')
     await page.click(settingsPage.contributeTab)
+    // NOTE: Wait for the navigation animation to finish
+    // Another variant is to disable animations via CSS: https://github.com/puppeteer/puppeteer/issues/511#issuecomment-468309571
+    // Or use fake timers: https://github.com/sinonjs/fake-timers
+    await waitFor(100)
 
     // It should look as expected.
     const screenshotPath = path.join(__dirname, 'contribute-view-default.expected.png')
@@ -56,6 +74,7 @@ describe('Settings view', function () {
     const page = await helper.openPage('settings')
     await page.click(settingsPage.darkModeToggle)
     await page.click(settingsPage.contributeTab)
+    await waitFor(100)
 
     // It should look as expected.
     const screenshotPath = path.join(__dirname, 'contribute-view-dark.expected.png')
