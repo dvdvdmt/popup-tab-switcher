aec1247bc5abf1865109be998a1809bd7fe3811b
Dmitriy Davydov <dvdvdmt@gmail.com>
2023-03-26 13:15:50 +0400

    Pass initial settings object to component.

--- src/settings/settings.tsx
+++ src/settings/settings.tsx
@@ -1,5 +1,30 @@
+import {render} from 'solid-js/web'
 import styles from './settings.module.scss'
+import {ServiceFactory} from '../service-factory'
+import {ISettings} from '../utils/settings'
 
-export function Settings() {
-  return <div class={styles.settings}>Test SolidJS</div>
+interface ISettingsProps {
+  settings: ISettings
+}
+
+export function Settings(props: ISettingsProps) {
+  const {settings} = props
+  return (
+    <div
+      class={`${styles.settings} mdc-typography`}
+      classList={{[styles.settings_dark]: settings.isDarkTheme}}
+    >
+      Test SolidJS
+    </div>
+  )
+}
+
+export async function initSettings() {
+  const settings = await ServiceFactory.getSettings()
+  window.settings = settings
+  // TODO: Render the app right into the body element
+  const appRootElement = document.createElement('div')
+  render(() => <Settings settings={settings} />, appRootElement)
+
+  document.body.appendChild(appRootElement)
 }
