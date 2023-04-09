fd359939ac03fd21b255280f72e500ba8dd2e884
Dmitriy Davydov <dvdvdmt@gmail.com>
2023-04-09 11:22:25 +0400

    Left a note about logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,6 @@
+// TODO: replace it with direct console.log calls and babel-plugin-transform-remove-debug
+// This log function has a drawback that it doesn't show the file name and line number in the
+// console of the place where it was called. It's more convenient to use console.log directly.
 export function log(...args: any): void {
   if (PRODUCTION) {
     return
