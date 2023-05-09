2754cf2b137510d76fe4a5813caa339014014c01
Dmitriy Davydov <dvdvdmt@gmail.com>
2023-05-09 07:39:36 +0400

    Use selector instead of concrete value

--- e2e/selectors/settings-page.ts
+++ e2e/selectors/settings-page.ts
@@ -2,4 +2,5 @@ export const settingsPage = {
   root: '[data-test="settings"]',
   darkModeToggle: '[data-test="darkModeToggle"]',
   contributeTab: '[data-test="contribute"]',
+  resetButton: '[data-test="resetButton"]',
 }
