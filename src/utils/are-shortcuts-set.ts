49615c3350a8fe0542e0e51c268d1173ce686b5a
Dmitriy Davydov <dvdvdmt@gmail.com>
2023-05-13 11:34:43 +0400

    Remove web extension polyfill from utils

--- src/utils/are-shortcuts-set.ts
+++ src/utils/are-shortcuts-set.ts
@@ -1,8 +1,7 @@
-import browser from 'webextension-polyfill'
 import {Command} from './constants'
 
 export default async function areShortcutsSet() {
-  const shortcuts = await browser.commands.getAll()
+  const shortcuts = await chrome.commands.getAll()
   const existingShortcuts = shortcuts.filter(({shortcut}) => shortcut)
   return existingShortcuts.length === Object.keys(Command).length
 }
