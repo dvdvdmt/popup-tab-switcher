a925b56106cbedcb3989bf0e01813e9d6968eec3
Dmitriy Davydov <dvdvdmt@gmail.com>
2024-06-23 14:53:50 +0200

    Reduce the number of iterations for performance measurements

--- e2e/features/performance/performance.test.ts
+++ e2e/features/performance/performance.test.ts
@@ -5,11 +5,10 @@ import {
   startPuppeteer,
   stopPuppeteer,
   timeoutDurationMS,
-  waitFor,
 } from '../../utils/puppeteer-utils'
 import {PuppeteerPopupHelper} from '../../utils/puppeteer-popup-helper'
 import {contentScript} from '../../selectors/content-script'
-import {getLogs, getRenderingTime} from '../../../src/utils/messages'
+import {getRenderingTime} from '../../../src/utils/messages'
 
 describe(`Sharing`, function () {
   let helper: PuppeteerPopupHelper
@@ -33,29 +32,20 @@ describe(`Sharing`, function () {
     // Scenario:
     // 1. Navigate to the page.
     // 2. Open the popup. And save the rendering time.
-    // 3. Repeat 1-2 for 100 times.
+    // 3. Repeat 1-2 for 50 times.
     // 4. Calculate the average rendering time.
     // 5. Assert that the average rendering time is less than 100ms.
-
-    const maxNumberOfIterations = 100
+    const maxNumberOfIterations = 50
     const maxRenderingTimeMs = 20
     const renderingTimes: number[] = []
-
     const page = await helper.openPage('stackoverflow.html')
-    do {
+    for (let i = 0; i < maxNumberOfIterations; i += 1) {
       await helper.selectTabForward()
-      try {
-        await page.isVisible(contentScript.root)
-      } catch (e) {
-        const logs = await helper.sendMessage(getLogs())
-        console.log(logs)
-        // The popup is not opened yet. Wait for the popup to be opened.
-        await waitFor()
-      }
+      await page.isVisible(contentScript.root)
       const renderingTime = await helper.sendMessage(getRenderingTime())
       renderingTimes.push(renderingTime)
       await helper.openPage('stackoverflow.html', page)
-    } while (renderingTimes.length < maxNumberOfIterations)
+    }
     const totalRenderingTime = renderingTimes.reduce((a, b) => a + b)
     const averageRenderingTimeMs =
       Math.round((totalRenderingTime / renderingTimes.length) * 100) / 100 // round number to 2 decimal places
