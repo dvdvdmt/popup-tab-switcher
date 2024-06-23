f550590719fe314715e28b9687d6c0eda141df16
Dmitriy Davydov <dvdvdmt@gmail.com>
2024-06-23 11:58:15 +0200

    Update both folders

--- scripts/update-screenshots.ts
+++ scripts/update-screenshots.ts
@@ -1,22 +1,27 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-const screenshotDir = path.join(__dirname, '..', 'e2e', 'features', 'settings-view')
+const screenshotDirs = [
+  path.join(__dirname, '..', 'e2e', 'features', 'settings-view'),
+  path.join(__dirname, '..', 'e2e', 'features', 'popup-view'),
+]
 
-fs.readdir(screenshotDir, (err, files) => {
-  if (err) {
-    console.error('Error reading directory:', err)
-    return
-  }
-
-  files.forEach((file) => {
-    if (file.endsWith('.current.png')) {
-      const baseName = file.replace('.current.png', '')
-      const currentPath = path.join(screenshotDir, file)
-      const expectedPath = path.join(screenshotDir, `${baseName}.expected.png`)
-
-      console.log(`Updating screenshot: ${expectedPath}`)
-      fs.copyFileSync(currentPath, expectedPath)
+screenshotDirs.forEach((screenshotDir) => {
+  fs.readdir(screenshotDir, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err)
+      return
     }
+
+    files.forEach((file) => {
+      if (file.endsWith('.current.png')) {
+        const baseName = file.replace('.current.png', '')
+        const currentPath = path.join(screenshotDir, file)
+        const expectedPath = path.join(screenshotDir, `${baseName}.expected.png`)
+
+        console.log(`Updating screenshot: ${expectedPath}`)
+        fs.copyFileSync(currentPath, expectedPath)
+      }
+    })
   })
 })
